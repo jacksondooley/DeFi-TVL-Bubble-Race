2e4d18a540665891d869cbbf56bfba751cfd8bbf
App Academy Student <email@email.com>
2022-04-06 11:34:45 -0400

    nan is moving

--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 import Example from "./scripts/functionality";
-import Fetch from "./scripts/fetch";
-import Bars from "./scripts/bars";
-import DataFetcher from "./scripts/data_fetcher";
-import barChart from "./scripts/barChart";
-import Data from "./data/dataGrouping";
+// import Fetch from "./scripts/fetch";
+// import Bars from "./scripts/bars";
+// import DataFetcher from "./scripts/data_fetcher";
+// import barChart from "./scripts/barChart";
+// import Data from "./data/dataGrouping";
+import { generateDataSets } from "./scripts/dataGenerator";
+import { BarChartRace } from "./scripts/barChartRace";
+
+import { select as d3Select } from "d3";
 // import dataGrouping from "./data/dataGrouping";
 
 document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main");
     new Example(main);
 
+    const myChart = new BarChartRace("bar-chart-race");
+
+    myChart
+      .setTitle("Bar Chart Race Title")
+      .addDatasets(generateDataSets({ size: 5 }))
+      .render();
     // let graph = new Bars();
 
     // graph.createSample();
@@ -26,10 +36,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // let race = new barChart("5", "yes");
     // console.log(race);
-    let chart = new Data();
+    // let chart = new Data();
     // data.testLogger();
-    chart.renderChart();
-    console.log("render time")
+    // chart.renderChart();
+    // console.log("render time")
     // console.log(data.width);
     // data.testLogger();
     // data.datesArr();
